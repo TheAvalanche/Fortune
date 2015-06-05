d0e2f82157f82f0cc1f384daf481993f57ecec9d
Alex Kartishev <alexkartishev@gmail.com>
2015-06-05 17:22:56 +0300

    Section and lines getting

--- src/main/java/org/fortune/service/BookParser.java
+++ src/main/java/org/fortune/service/BookParser.java
@@ -5,7 +5,12 @@ import org.fortune.schema._2_0.PoemType;
 import org.fortune.schema._2_0.SectionType;
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,8 +34,36 @@ public class BookParser {
                 .collect(Collectors.toList());
     }
 
-    public String findLine(FictionBook fictionBook, int page, int line) {
-        return (String) fictionBook.getBody().get(0).getSection().get(page).getPoem().getStanza().get(line).getV().get(line).getContent().get(0);
+    public List<String> getLines(FictionBook fictionBook) {
+
+        return getSections(fictionBook).stream()
+                .filter(section -> section.getPoem() != null)
+                .map(s -> s.getPoem().getStanza()).flatMap(Collection::stream)
+                .map(PoemType.Stanza::getV).flatMap(Collection::stream)
+                .map(p -> (String) p.getContent().get(0))
+                .collect(Collectors.toList());
+
+    }
+
+    public List<SectionType> getSections(FictionBook fictionBook) {
+        List<SectionType> sections = new LinkedList<>();
+        fictionBook.getBody().stream()
+                .map(FictionBook.Body::getSection)
+                .flatMap(Collection::stream).forEach(s -> addSubsections(s, sections));
+        return sections;
+    }
+
+    private void addSubsections(SectionType section, List<SectionType> sectionTypes) {
+        if (section.getSection() != null) {
+            section.getSection().stream().forEach(s -> addSubsections(s, sectionTypes));
+        }
+        sectionTypes.add(section);
+    }
+
+    public FictionBook stringToBook(String book) throws JAXBException {
+        JAXBContext c = JAXBContext.newInstance(FictionBook.class);
+        Unmarshaller unmarshaller = c.createUnmarshaller();
+        return (FictionBook) unmarshaller.unmarshal(new StringReader(book));
     }
 
 }
--- src/test/java/org/fortune/service/BookParserTest.groovy
+++ src/test/java/org/fortune/service/BookParserTest.groovy
@@ -1,10 +1,6 @@
 package org.fortune.service
-
 import org.fortune.schema._2_0.FictionBook
 import spock.lang.Specification
-
-import javax.xml.bind.JAXBContext
-import javax.xml.bind.Unmarshaller
 
 class BookParserTest extends Specification {
 
@@ -12,24 +8,52 @@ class BookParserTest extends Specification {
 
     BookParser bookParser = new BookParser()
 
-    def setup() {
-        JAXBContext c = JAXBContext.newInstance(FictionBook.class)
-        Unmarshaller unmarshaller = c.createUnmarshaller()
-        fictionBook = (FictionBook) unmarshaller.unmarshal(this.class.getResourceAsStream("/Onegin.fb2"))
+    def testPoem = '''<?xml version="1.0" encoding="utf-8"?>
+                        <FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" xmlns:l="http://www.w3.org/1999/xlink">
+                          <body>
+                            <section>
+                              <section>
+                                  <poem>
+                                    <stanza>
+                                      <v>Test1</v>
+                                    </stanza>
+                                  </poem>
+                              </section>
+                            </section>
+                            <section>
+                              <poem>
+                                <stanza>
+                                  <v>Test1</v>
+                                  <v>Test2</v>
+                                </stanza>
+                              </poem>
+                            </section>
+                          </body>
+                          <body>
+                            <section>
+                              <poem>
+                                <stanza>
+                                  <v>Test1</v>
+                                </stanza>
+                              </poem>
+                            </section>
+                          </body>
+                        </FictionBook>'''
+
+    def "should get all sections with subsections"() {
+        when:
+        FictionBook fictionBook = bookParser.stringToBook(testPoem)
+        def sections = bookParser.getSections(fictionBook)
+        then:
+        sections.size() == 4
     }
 
-    def "Should return the first line"() {
+    def "should get all poem lines"() {
         when:
-        def line = bookParser.findLine(fictionBook, 0, 1)
+        FictionBook fictionBook = bookParser.stringToBook(testPoem)
+        def lines = bookParser.getLines(fictionBook)
         then:
-        line == "He мысля гордый свет забавить,"
-    }
-
-    def "Should add all lines to list"() {
-        when:
-        def lines = bookParser.parseLines(fictionBook)
-        then:
-        lines.size() == 91
+        lines.size() == 4
     }
 
 }
